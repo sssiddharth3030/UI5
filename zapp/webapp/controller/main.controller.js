94e35d71bed72939f61938eb8cf5d2b1a04ce5ed
sssiddharth3030 <sssiddharth3030@gmail.com>
2025-05-29 01:14:26 +0530

    add

--- zapp/webapp/controller/main.controller.js
+++ zapp/webapp/controller/main.controller.js
@@ -5,6 +5,7 @@ sap.ui.define(
 
         return Controller.extend("zapp.opensource.com.zapp.controller.main", {
             onInit() {
+                this.getView().byId("user").setEditable(false);
                 this.getView().byId("name").setEditable(false);
                 this.getView().byId("street").setEditable(false);
                 this.getView().byId("city").setEditable(false);
@@ -14,8 +15,6 @@ sap.ui.define(
             onCheck() {
 
                 const getData = async (username) => {
-                    // var get = await fetch(`https://jsonplaceholder.typicode.com/users`)
-
                     const response = await fetch(`https://jsonplaceholder.typicode.com/users?username=${username}`)
 
                     if (!response.ok) {
@@ -28,23 +27,10 @@ sap.ui.define(
                     
                     return user[0] ? user : -1
 
-                    // fetch(`https://jsonplaceholder.typicode.com/users?username=${username}`)
-                    //     .then(response => {
-                    //         console.log(`response:${response.json()}`)
-                    //         response.json()
-                    //     })
-                    //     .then(user => { 
-                    //         console.log(`user: ${user}` )  
-                    //         return user
-                    //     })
-                    //     .catch(error => { 
-                    //         console.log(error) 
-                    //         return error 
-                    //     })
                 }
 
                 const displayData = (oView, user) => {
-
+                    oView.byId("user").setValue(user[0].username);
                     oView.byId("name").setValue(user[0].name);
                     oView.byId("street").setValue(user[0].address.street);
                     oView.byId("city").setValue(user[0].address.city);
@@ -55,6 +41,7 @@ sap.ui.define(
                 }
 
                 const displayError = (oView, username) => {
+                    oView.byId("user").setValue('');
                     oView.byId("name").setValue('');
                     oView.byId("street").setValue('');
                     oView.byId("city").setValue('');
